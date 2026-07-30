0ea8e1408acb8953c6873e97353f6b44ba3bb6bc
Jay <blue850523@gmail.com>
2026-07-30 17:11:56 +0800

    fix: Code Splitting 調整

--- vite.config.ts
+++ vite.config.ts
@@ -30,19 +30,20 @@ export default defineConfig({
     rollupOptions: {
       output: {
         /** 
-         * 自訂打包拆分策略, 本來只有一個index-[hash].js, 切分成vendor-vue-[hash].js、vendor-libs-[hash].js, 如果有用到非同步組件也會被拆開來
+         * 自訂打包拆分策略, 本來只有一個index-[hash].js, 切分成vendor-vue-[hash].js、vendor-libs-[hash].js, 如果有用到非同步組件也會被拆開
+         * 這邊先註解不用再拆, 要的話vue, pinia, Element Plus都要綁一塊, 除非有大型套件才要再分
         */
-        manualChunks(id) {
-          // 如果引用的模組來自 node_modules
-          if (id.includes('node_modules')) {
-            // 把 vue, vue-router, pinia 等核心庫打包成一個叫 vendor-vue 的檔案
-            if (id.includes('vue') || id.includes('pinia')) {
-              return 'vendor-vue'
-            }
-            // 其他第三方庫（例如 axios 等）打包成另一個檔案
-            return 'vendor-libs'
-          }
-        },
+        // manualChunks(id) {
+        //   // 如果引用的模組來自 node_modules
+        //   if (id.includes('node_modules')) {
+        //     // 把 vue, vue-router, pinia 等核心庫打包成一個叫 vendor-vue 的檔案
+        //     if (id.includes('vue') || id.includes('pinia')) {
+        //       return 'vendor-vue'
+        //     }
+        //     // 其他第三方庫（例如 axios 等）打包成另一個檔案
+        //     return 'vendor-libs'
+        //   }
+        // },
         // 讓打包後的檔名結構更好看, 加上 hash 防止瀏覽器快取舊檔案
         chunkFileNames: 'assets/js/[name]-[hash].js',
         entryFileNames: 'assets/js/[name]-[hash].js',
